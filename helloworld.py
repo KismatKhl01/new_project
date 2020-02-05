3a326a4cb14ca101ae34b40ef150e99c26ad4771
kismatkhl01 <kismatkhl@gmail.com>
2020-02-05 19:00:22 +0545

    add substraction and addition function in helloworld

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,10 @@
-def printdata():
-   print("hell")
+def add(a,b):
+   return a+b
+result=add(2,3)
+print("Sum : ",result)
+def sub(a,b):
+   return a-b
+result=sub(5,4)
+print("Substraction : ",result)
 
-print("hello world !!!")
 
